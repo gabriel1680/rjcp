c6a67c1f251f40d0466a09ad556c88bfdd035234
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2026-03-29 18:45:23 -0300

    feat: add ClientMain

---
+++ src/main/java/org/gbl/ClientMain.java
@@ -0,0 +1,20 @@
+package org.gbl;
+
+import org.apache.log4j.BasicConfigurator;
+import org.gbl.protocol.rjcp.RJCP;
+import org.gbl.transport.RPCClient;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class ClientMain {
+    public static void main(String[] args) throws IOException {
+        BasicConfigurator.configure();
+        try (var client = new RPCClient("localhost", 8080, new RJCP())) {
+            client.ping();
+            final var bytes = client.sendMessage("Hello!");
+            System.out.println("Server response:" + new String(bytes, StandardCharsets.UTF_8));
+        }
+        System.out.println("request ended");
+    }
+}
---
+++ src/main/java/org/gbl/application/DummyHandler.java
@@ -0,0 +1,41 @@
+package org.gbl.application;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.gbl.protocol.MessageType;
+import org.gbl.protocol.NetworkProtocol;
+import org.gbl.protocol.RPCMessage;
+import org.gbl.transport.MessageHandler;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+public class DummyHandler implements MessageHandler {
+
+    private static final Logger LOG = LogManager.getLogger(DummyHandler.class);
+
+    @Override
+    public void handle(NetworkProtocol protocol, RPCMessage message, OutputStream out) throws IOException {
+        switch (message.type()) {
+            case PING -> {
+                try {
+                    LOG.debug("PING received");
+                    protocol.send(out, MessageType.PONG, new byte[0]);
+                    out.flush();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            case MESSAGE -> {
+                final var text = new String(message.data(), StandardCharsets.UTF_8);
+                LOG.debug("MESSAGE: " + text);
+                protocol.send(out, MessageType.MESSAGE, "ACK");
+            }
+            case PONG -> LOG.warn("PONG received (unexpected)");
+            case ERROR -> LOG.error("ERROR: " + new String(message.data(),
+                                                           StandardCharsets.UTF_8));
+            default -> protocol.send(out, MessageType.ERROR, "Unknown message type");
+        }
+    }
+}
