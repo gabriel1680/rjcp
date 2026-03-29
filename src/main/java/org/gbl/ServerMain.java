851129a10c895d1d2aa5500c113e82c18110fba2
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2026-03-29 18:45:03 -0300

    feat: add ServerMain

---
+++ src/main/java/org/gbl/ServerMain.java
@@ -0,0 +1,16 @@
+package org.gbl;
+
+import org.apache.log4j.BasicConfigurator;
+import org.gbl.protocol.rjcp.RJCP;
+import org.gbl.application.DummyHandler;
+import org.gbl.transport.RPCServer;
+
+public class ServerMain {
+    public static void main(String[] args) {
+        BasicConfigurator.configure();
+        final var protocol = new RJCP();
+        final var handler = new DummyHandler();
+        final var server = new RPCServer(protocol, handler);
+        server.start(8080);
+    }
+}
