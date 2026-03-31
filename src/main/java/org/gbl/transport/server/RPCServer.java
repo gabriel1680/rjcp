c8ad19abd8da562e992c06bd0d5dd424bc26b574
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2026-03-30 22:13:23 -0300

    refactor: make running visible to threads and reinforce pre-state conditions

--- src/main/java/org/gbl/transport/server/RPCServer.java
+++ src/main/java/org/gbl/transport/server/RPCServer.java
@@ -16,16 +16,17 @@ public class RPCServer {
 
     private final RCPConnectionFactory connectionFactory;
     private final MessageHandler handler;
+    private final ExecutorService executor;
 
-    private boolean running;
+    private volatile boolean running;
 
-    private ServerSocket serverSocket;
-    private ExecutorService executor;
+    private ServerSocket server;
 
     public RPCServer(RCPConnectionFactory connectionFactory, MessageHandler handler) {
         this.connectionFactory = connectionFactory;
         this.handler = handler;
         this.running = false;
+        this.executor = Executors.newVirtualThreadPerTaskExecutor();
     }
 
     public boolean isRunning() {
@@ -33,45 +34,65 @@ public class RPCServer {
     }
 
     public void stop() {
+        if (!running) return;
+
         running = false;
         try {
-            if (serverSocket != null) serverSocket.close();
-            if (executor != null) executor.shutdown();
+            server.close();
+            executor.shutdown();
         } catch (Exception e) {
             LOG.error("Error on stopping the server", e);
+            throw new RuntimeException("Failed to stop server", e);
         }
     }
 
     public void start(int port) {
-        running = true;
-        try (var server = new ServerSocket(port)) {
-            serverSocket = server;
-            LOG.info("Server running on port:" + port);
-            try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
-                this.executor = executor;
-                while (running) {
-                    final var socket = server.accept();
-                    executor.submit(() -> handle(socket));
-                }
-            } catch (IOException e) {
-                LOG.error("Failed during execution", e);
-            }
+        if (running)
+            throw new IllegalStateException("server already running");
+
+        try {
+            running = true;
+            server = new ServerSocket(port);
         } catch (IOException e) {
             LOG.error("Failed to create the socket", e);
+            throw new RuntimeException("Failed to create server", e);
+        }
+
+        LOG.info("Server is running on %s".formatted(toCanonicalAddress(server)));
+        while (running) {
+            try {
+                final var socket = server.accept();
+                executor.submit(() -> handle(socket)); // TODO: limit VT creation - semaphore?
+            } catch (IOException e) {
+                if (running) {
+                    LOG.error("Failed to accept new connection", e);
+                } else {
+                    LOG.debug("Server socket closed, stopping accept loop...");
+                }
+            }
         }
     }
 
     private void handle(Socket socket) {
-        LOG.info("Client connected: " + socket.getInetAddress());
-        try (var connection = connectionFactory.createFrom(socket)) {
+        final var canonicalAddress = toCanonicalAddress(socket);
+        LOG.info("Client connected: %s".formatted(canonicalAddress));
+        try (socket; var connection = connectionFactory.createFrom(socket)) {
             while (running) {
                 final var message = connection.receive();
                 handler.handle(message, connection);
             }
         } catch (EOFException e) {
-            LOG.info("Client disconnected: " + socket.getInetAddress() + ":" + socket.getPort());
+            LOG.info("Client disconnected: %s".formatted(canonicalAddress));
         } catch (Exception e) {
-            LOG.error("Error with client: " + socket, e);
+            LOG.error("Error with client: %s".formatted(socket), e);
         }
+    }
+
+    private static String toCanonicalAddress(Socket socket) {
+        return "%s:%d".formatted(socket.getInetAddress(), socket.getPort());
+    }
+
+    private static String toCanonicalAddress(ServerSocket socket) {
+        return "%s:%d".formatted(socket.getInetAddress(), socket.getLocalPort());
     }
 }
